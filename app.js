168939ae38f32cb63713b662020ba329ef919eaf
Pallavi Dhadda <85661960+pallavidhadda@users.noreply.github.com>
2021-11-01 11:26:49 +0530

    add files

---
+++ app.js
@@ -0,0 +1,7 @@
+lottie.loadAnimation({
+    container: document.getElementById('bm'), // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: false,
+    autoplay: true,
+    path: 's-logo-new-1.json' // the path to the animation json
+});
